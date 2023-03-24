8058719808cbacdd3d3eb64f062539144d10a3e6
Artem Udovyk <udovyk.a@konstankino.com>
2023-03-24 14:09:10 +0200

    feat: check for wildcard resources

--- src/main.py
+++ src/main.py
@@ -12,6 +12,22 @@ def find_allowed_resources(policies: list[dict], target_action: str) -> set[str]
     return set(allowed_resources)
 
 
+def find_allowed_resources_subset(resources: list[str], allowed_resources: set[str]) -> set[str]:
+    allowed_resources_subset = []
+    wildcard_allowed_resources = [allowed_resource for allowed_resource in allowed_resources if "*" in allowed_resource]
+
+    for resource in resources:
+        if resource in allowed_resources:
+            allowed_resources_subset.append(resource)
+        
+        # Check if the resource matches the wildcard allowed resource
+        for wildcard_allowed_resource in wildcard_allowed_resources:
+            if resource.startswith(wildcard_allowed_resource.replace("*", "")):
+                allowed_resources_subset.append(resource)
+            
+    return set(allowed_resources_subset)
+
+
 if __name__ == "__main__":
     policies = [
         {
@@ -22,10 +38,16 @@ if __name__ == "__main__":
                 "s3-object-lambda:Get*",
                 "s3-object-lambda:List*",
             ],
-            "Resource": ["resource1", "resource2", "resource3"],
+            "Resource": ["resource_wildcard*", "resource2", "resource3"],
         }
     ]
 
     target_action = "s3:GetObject"
     allowed_resources = find_allowed_resources(policies, target_action)
     print("allowed_resources:", allowed_resources)
+    
+    resources = ["resource_wildcard1", "resource_wildcard2", "resource2", "resource3", "resource4", "definitely_not_allowed"]
+    
+    allowed_resources_subset = find_allowed_resources_subset(resources, allowed_resources)
+    print("all resources:", resources)
+    print("allowed_resources_subset:", allowed_resources_subset)
